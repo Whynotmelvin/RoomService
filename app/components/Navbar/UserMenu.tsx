b5eaa0a55dac5a76c18c0d6b174eccc7d7fe7de4
Melvin <melvinundercover@icloud.com>
2023-12-03 20:01:51 +0100

    Navbar dropdown

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -2,10 +2,16 @@
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useAmp } from "next/amp";
-import { useState } from "react";
+import { useState, useCallback } from "react";
+import Menuitem from "./Menuitem";
 
 const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
@@ -26,7 +32,9 @@ const UserMenu = () => {
           Airbnb your home
         </div>
         <div
-          onClick={() => {}}
+          onClick={() => {
+            toggleOpen;
+          }}
           className="
          p-4
          md:py-1
@@ -50,6 +58,29 @@ const UserMenu = () => {
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div
+          className="
+            absolute
+             rounded-xl
+             shadow:md
+             w-[40vw]
+             md:w-3/4
+             bg-white
+             overflow-hidden
+             right-0
+             top-12
+             text-sm"
+        >
+          <div
+            className="flex flex-col cursor-pointer"
+          >
+            <>
+              <Menuitem onClick={() => {}} label="Login" />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
